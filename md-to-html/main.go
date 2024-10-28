c2fcef4ffb66507011bc5582877e6cc46aaff453
Shrinav Loka <lokashrinav@gmail.com>
2024-10-28 12:14:32 -0400

    Finished Markdown to HTML Scanner

--- md-to-html/main.go
+++ md-to-html/main.go
@@ -1,5 +1,27 @@
 package main
 
 import (
-	
+	"fmt"	
+	"os"
+	"bufio"
+	"github.com/gomarkdown/markdown"
 )
+
+func main() {
+
+	fileName := "smth.md"
+
+	file, _ := os.Open(fileName)
+
+	scanner := bufio.NewScanner(file)
+
+	var markdownText string = ""
+
+	for scanner.Scan() {
+		markdownText += scanner.Text() + "\n"
+	}
+
+	markdown1 := markdown.ToHTML([]byte(markdownText), nil, nil)
+
+	fmt.Println(string(markdown1))
+}
