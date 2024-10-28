ae7caada0bc3de1ee932cd819fce67fa94d777dd
Shrinav Loka <lokashrinav@gmail.com>
2024-10-28 13:44:41 -0400

    Added

--- weather/main.go
+++ weather/main.go
@@ -1,10 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"net/http"
-	"os"
+	//"os"
+	"encoding/json"
 )
+
+type WeatherData struct {
+    Location struct {
+        Name string `json:"name"` 
+    } `json:"location"`
+    Current struct {
+        TempF    float64 `json:"temp_f"`
+        Humidity int     `json:"humidity"`
+        Condition struct {
+            Text string `json:"text"`
+        } `json:"condition"` 
+    } `json:"current"`
+}
 
 func main() {
 	api_key := "5facba76f0064528bb4162342242810"
@@ -13,7 +28,7 @@ func main() {
 		return
 	}
 	defer request.Body.Close()
-	newFile, err2 := os.Create("smth.json")
+	/*newFile, err2 := os.Create("smth.json")
 	if err2 != nil {
 		return
 	}
@@ -21,6 +36,17 @@ func main() {
 	_, err3 := io.Copy(newFile, request.Body)
 	if err3 != nil {
 		return
-	}
+	}*/
+	body, err := io.ReadAll(request.Body)
 
+	var x WeatherData
+    err2 := json.Unmarshal([]byte(body), &x)
+	if err2 != nil {
+		return
+	}
+	
+	fmt.Println("Location:", x.Location.Name)
+	fmt.Printf("Temperature: %.2f°F\n", x.Current.TempF)
+	fmt.Println("Humidity:", x.Current.Humidity)
+	fmt.Println("Condition:", x.Current.Condition.Text)
 }
