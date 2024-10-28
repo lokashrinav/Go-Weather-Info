411eb8f25204bfa1b027115117fe66da6bcc1beb
Shrinav Loka <lokashrinav@gmail.com>
2024-10-28 13:10:07 -0400

    Downloaded Json to Get Format of Response Body

--- weather/main.go
+++ weather/main.go
@@ -1,7 +1,26 @@
 package main
 
 import (
-	"fmt"
+	"io"
 	"net/http"
-	"encoding/json"
+	"os"
 )
+
+func main() {
+	api_key := "5facba76f0064528bb4162342242810"
+	request, err := http.Get("http://api.weatherapi.com/v1/current.json?key=" + api_key + "&q=London")
+	if err != nil {
+		return
+	}
+	defer request.Body.Close()
+	newFile, err2 := os.Create("smth.json")
+	if err2 != nil {
+		return
+	}
+	defer newFile.Close()
+	_, err3 := io.Copy(newFile, request.Body)
+	if err3 != nil {
+		return
+	}
+
+}
